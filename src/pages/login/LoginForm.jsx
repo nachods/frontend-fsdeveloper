490d206a49172a6f86b3dce5801b890a92bb1eaf
nachods <nachodesimone36@gmail.com>
2024-08-07 17:59:03 -0300

    Login Fronted Terminado

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png';
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
+    <div className={styles.gradientcontainer}>
+      <div className={styles.contform}>
+        <div className={styles.contlogo}>
+          <img className={styles.logo} src={Logo} alt='logo'></img>
+        </div>
+        <input
+          type="email"
+          name="email"
+          placeholder="Correo Electrónico"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+        />
+        <button>Iniciar Sesión</button>
+        <p>
+        ¿No tienes una cuenta? <Link className={styles.link} to="/">Registrate ahora</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm
--- src/pages/register/RegistrationForm.jsx
+++ src/pages/register/RegistrationForm.jsx
@@ -16,6 +16,16 @@ const RegistrationForm = () => {
           placeholder="Nombre"
         />
         <input
+          type="text"
+          name="lastname"
+          placeholder="Apellido"
+        />
+        <input
+          type="number"
+          name="lastname"
+          placeholder="Teléfono"
+        />
+        <input
           type="email"
           name="email"
           placeholder="Correo Electrónico"
@@ -27,7 +37,7 @@ const RegistrationForm = () => {
         />
         <button>Registrarse</button>
         <p>
-        ¿Ya tienes una cuenta? <Link className={styles.link} to="/login">Iniciar sesión</Link>
+          ¿Ya tienes una cuenta? <Link className={styles.link} to="/login">Iniciar sesión</Link>
         </p>
       </div>
     </div>
