34caaf101d505b29de78aee89f58b4b6b589140d
nachods <nachodesimone36@gmail.com>
2024-08-07 17:48:07 -0300

    Register Front Terminado

--- src/pages/register/RegistrationForm.jsx
+++ src/pages/register/RegistrationForm.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import styles from './Registration.module.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png';
 
 const RegistrationForm = () => {
   return (
     <div className={styles.gradientcontainer}>
-      <div>
-				<input
-					type="text"
-					name="firstname"
-					placeholder="Nombre"
-				/>
+      <div className={styles.contform}>
+        <div className={styles.contlogo}>
+          <img className={styles.logo} src={Logo} alt='logo'></img>
+        </div>
         <input
-					type="email"
-					name="email"
-					placeholder="Correo Electronico"
-				/>
+          type="text"
+          name="firstname"
+          placeholder="Nombre"
+        />
         <input
-					type="password"
-					name="password"
-					placeholder="Contraseña"
-				/>
-        
-			</div>
-    </div>  )
+          type="email"
+          name="email"
+          placeholder="Correo Electrónico"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+        />
+        <button>Registrarse</button>
+        <p>
+        ¿Ya tienes una cuenta? <Link className={styles.link} to="/login">Iniciar sesión</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
