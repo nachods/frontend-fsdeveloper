347fd0a13c6802b4fa50b2a6e7c2cf800aa36098
nachods <nachodesimone36@gmail.com>
2024-08-26 12:39:01 -0300

    Register Andando

---
+++ src/api/registerFetch.js
@@ -0,0 +1,20 @@
+export const registerFetch = async (data) => {
+  try {
+    const url = "http://localhost:3977/api/v1/auth/register"; // Donde envio los datos
+
+    const params = {
+      method: 'POST', // Tipo de solicitud HTTP
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // Cadena Js a JSON, convierto el usuario
+    };
+
+    const response = await fetch(url, params); // Envio la data a través del fetch
+    const result = await response.json(); // Analiza respuesta pasando de JSON a Js
+    if (response.status !== 200) throw result;
+    return result; // Si es exitoso, devuelve el objeto Js con la info del nuevo usuario
+  } catch (error) {
+    throw error;
+  }
+};
--- src/pages/register/RegistrationForm.jsx
+++ src/pages/register/RegistrationForm.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Registration.module.css';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
+import { registerFetch } from '../../api/registerFetch';
 
 const RegistrationForm = () => {
+  /* 
+  datos del formulario
+   */
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+  });
+
+  /* 
+validacion de formulario
+ */
+  const [error, setError] = useState(null); //Estado de los msjs de error o correcto
+  const [success, setSuccess] = useState(false);
+
+  const handleInputChange = (e) => { //Cambia los datos por los que manda el usuario
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  /* 
+  obtener los datos del formulario de registro
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // data
+    try {
+      const res = await registerFetch(formData); // Enviar datos del formulario
+      console.log(res);
+      setError('');
+      setSuccess(res.msg); // Msg proveniente del backend
+    } catch (error) {
+      console.log(error);
+      setError(error.msg); // Msg proveniente del backend
+      setSuccess('');
+    }
+  };
+  
+
+
   return (
     <div className={styles.gradientcontainer}>
-      <form className={styles.contform}>
+      <form className={styles.contform} onSubmit={handleSubmit}>
         <div className={styles.contlogo}>
           <img className={styles.logo} src={Logo} alt='logo'></img>
         </div>
@@ -14,33 +59,38 @@ const RegistrationForm = () => {
           type="text"
           name="firstname"
           placeholder="Nombre"
+          value={formData.firstname}
+          onChange={handleInputChange}
         />
         <input
           type="text"
           name="lastname"
           placeholder="Apellido"
-        />
-        <input
-          type="number"
-          name="lastname"
-          placeholder="Teléfono"
+          value={formData.lastname}
+          onChange={handleInputChange}
         />
         <input
           type="email"
           name="email"
           placeholder="Correo Electrónico"
+          value={formData.email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           name="password"
           placeholder="Contraseña"
+          value={formData.password}
+          onChange={handleInputChange}
         />
+        {error && <p className="alert alert-danger">{error}</p>}
         <button type='submit'>Registrarse</button>
+        {success && <p className="alert alert-success">Registro Completado</p>}
         <p>
           ¿Ya tienes una cuenta? <Link className={styles.link} to="/login">Iniciar sesión</Link>
         </p>
         <div className={styles.contcontc}>
-          <i className="bi bi-whatsapp"></i> 
+          <i className="bi bi-whatsapp"></i>
           <i className="bi bi-instagram"></i>
         </div>
       </form>
